244d536230fd99ed0535ffb3fe55fa820aaf9ccd
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-01 19:33:00 +0530

    IO constructor implemented

--- src/app/filehandling/IO.cpp
+++ src/app/filehandling/IO.cpp
@@ -1,4 +1,11 @@
 #include<bits/stdc++.h>
-#include<IO.hpp>
+#include"IO.hpp"
 
 using namespace std;
+
+IO::IO(const std ::string &file_path){
+    file_stream.open(file_path, std::ios::in | std::ios::out | std::ios::binary);
+    if(!file_stream.is_open()){
+      std::cout << "Unable to open the file " <<file_path<<std::endl; 
+    }
+}
