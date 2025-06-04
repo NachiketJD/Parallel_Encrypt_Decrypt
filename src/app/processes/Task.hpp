0fcc47a17af9a97afe4dcb722ba51f506675cdf6
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-04 17:14:59 +0530

    Implemented Task Struct

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -19,6 +19,39 @@ struct Task{
         action = act;;
         filePath = filepath;
     } //&& no alag se copy banegi// TASK CONSRTUCTOR
+
+    std::string toString(){
+        std::ostringstream oss;
+        oss<<filePath<<","<<(action==Action::ENCRYPT? "ENCRYPT": " DECRYPT");
+        return oss.str();
+    }
+    //agar mera koi test.txt file ko encry karna hai,
+    //  uska ek task bana hua hoga by task struct jisme
+    //  file path, action, file stream hoga
+    //   IO se us file ko khol ke launga uski yaha pe filestream store hogi
+    // Sabse pehle vo ek stream banayega, usme filePath ko push karega, folleowed by a comma
+    //  and then action (ency/decry)
+
+    static Task fromString(const std::string &taskData){
+        std::istringstream iss(taskData);
+        std::string filePath , actionStr;
+
+        if(std::getline(iss, filePath, ',') && std:: getline(iss, actionStr)){
+            Action action = (actionStr == "ENCRYPT")? Action::ENCRYPT : Action::DECRYPT;
+            IO io(filePath);
+            std::fstream f_stream = std:: move(io.getFileStream());
+            if(f_stream.is_open()){
+                return Task (std::move(f_stream), action, filePath);
+            }else{
+                throw std ::runtime_error("File not found " + filePath);
+            }
+        }
+        else{
+            throw std:: runtime_error("Invalid task data format");
+        }
+    }
+
+
 };
 
 #endif
