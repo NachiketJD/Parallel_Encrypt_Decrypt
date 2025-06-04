9223a7c08712f4509d6730752b5b39a1372e0371
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-04 11:06:15 +0530

    Implemented ReadEnv.cpp

--- src/app/filehandling/ReadEnv.cpp
+++ src/app/filehandling/ReadEnv.cpp
@@ -5,7 +5,14 @@ class ReadEnv
 {
 
 public:
-    ReadEnv(/* args */);
-    ~ReadEnv();
+    std::string getenv(){
+        std:: string env_path=".env";
+        IO io(env_path);
+        std::fstream f_stream =  io.getFileStream(); // Filestream jo open ki hai vo return karega
+        std:: stringstream buffer;
+        buffer << f_stream.rdbuf();
+        std::string content = buffer.str();
+        return content;
+    }
 };
 
