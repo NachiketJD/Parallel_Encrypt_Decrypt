879df9d25c1f850ba01439daa745f63900e2d2a8
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-04 17:20:28 +0530

    Created PM class

---
+++ src/app/processes/ProcessManagement.hpp
@@ -0,0 +1,24 @@
+#ifndef PROCESS_MANAGEMENT_HPP
+#define PROCESS_MANAGEMENT_HPP
+
+#include<bits/stdc++.h>
+#include"Task.hpp"
+
+class ProcessManagement
+{
+public:
+    ProcessManagement();
+    bool submitToQueue(std :: unique_ptr<Task> task);
+    void executeTasks();
+    private:
+    std :: queue<std::unique_ptr<Task>> taskQueue;
+};
+
+ProcessManagement::ProcessManagement(/* args */)
+{
+}
+
+
+
+
+#endif
