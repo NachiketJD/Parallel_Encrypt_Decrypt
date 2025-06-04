9ddb63ffbc1bfb7713f25850a366054f9b592a03
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-04 16:55:10 +0530

    Task constructor initialized

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -1,11 +1,24 @@
 #ifndef TASK_HPP
 #define TASK_HPP
 
+#include<bits/stdc++.h>
+#include"../filehandling/IO.hpp"
+
 enum class Action{
     ENCRYPT,
     DECRYPT,
 };
 
+struct Task{
+    std::string filePath;
+    std:: fstream f_stream;
+    Action action;
 
+    Task(std::fstream &&stream, Action act, std::string filepath){
+        f_stream = std::move(stream);
+        action = act;;
+        filePath = filepath;
+    } //&& no alag se copy banegi// TASK CONSRTUCTOR
+};
 
 #endif
